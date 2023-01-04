a7d03acbf72ebe655d2e4a8aa31139398cbedf78
Jay Medina <jay.medina.celis@gmail.com>
2023-01-04 14:21:10 -0500

    Add leading zeros to time

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ function time() {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
   return hours + ":" + minutes + ":" + seconds;
 }
 
